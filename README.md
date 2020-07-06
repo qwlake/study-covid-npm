5283364ca7b3814e2d52d639e68b870cf18c907c
jungwoo <qwlake@gmail.com>
2020-07-06 15:44:56 +0900

    feat: 초기 사용 예제 추가

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-study-covid-npm
+# covid19-kr
+
+대한민국 코로나19 바이러스 감염자수 조회 API 사용 예제 with Node.js
+
+[https://www.data.go.kr/tcs/dss/selectApiDataDetailView.do?publicDataPk=15043376](https://www.data.go.kr/tcs/dss/selectApiDataDetailView.do?publicDataPk=15043376)
+
+![image](https://user-images.githubusercontent.com/41278416/86562981-8d9f8080-bf9e-11ea-9b99-000b33756761.png)
---
+++ index.js
@@ -0,0 +1,45 @@
+﻿const parseString = require('xml2js').parseString;
+const request = require('request');
+const secret = require('./secret.json');
+const express = require('express');
+const app = express();
+
+function requestCovid(url, queryParams) {
+    return new Promise(
+        function(resolve, reject) {
+            let header = '';
+            let body = '';
+            request.get(url+queryParams, (covidErr, covidRes, covidBody)=> {
+                if(covidErr) {
+                    console.log(`covidErr => ${covidErr}`)
+                } else if(covidRes.statusCode == 200) {
+                    parseString(covidBody, function(parseErr, parseRes) {
+                        let response = parseRes.response;
+                        header = response.header;
+                        body = response.body;
+                        resolve(body);
+                    }); 
+                }
+            })
+        }
+    )
+}
+
+app.get('/', function(req, res) {
+    let url = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson'; /*URL*/
+    let queryParams = '?' + encodeURIComponent('ServiceKey') + '='+secret.covid19; /*Service Key*/
+    queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /*페이지 번호*/
+    queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /*한 페이지 결과 수*/
+    queryParams += '&' + encodeURIComponent('startCreateDt') + '=' + encodeURIComponent('20200310'); /*데이터 생성일 시작범위*/
+    queryParams += '&' + encodeURIComponent('endCreateDt') + '=' + encodeURIComponent('20200315'); /*데이터 생성일 종료범위*/
+
+    let getCovid = async function() { 
+        res.json( await requestCovid(url, queryParams));
+    }
+    getCovid();
+});
+
+var port = 3000;
+app.listen(port, function(){
+    console.log('server on! http://localhost:'+port);
+});
